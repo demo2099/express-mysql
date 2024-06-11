e3c523fe5c9908d3cc94f7f9fb3159b63e95cde0
demo <available3099@gmail.com>
2024-06-11 20:16:50 +0800

    修改可用docker

--- routes/link.js
+++ routes/link.js
@@ -38,7 +38,6 @@ let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-http://789258.xyz/link/dkuucIduUEN8tnv6?sub=3
 trojan://402d7490-6d4b-42d4-80ed-e681b0e6f1f9@cwork.678567.xyz:443?security=tls&type=ws&host=cwork.678567.xyz&path=%2F#USC
 trojan://402d7490-6d4b-42d4-80ed-e681b0e6f1f9@cwork.678567.xyz:443?security=tls&type=ws&host=cwork.678567.xyz&path=%2F#USC
 vless://402d7490-6d4b-42d4-80ed-e681b0e6f1f7@jp99.987443.xyz:443?encryption=none&security=tls&sni=jp99.987443.xyz&fp=randomized&type=ws&host=jp99.987443.xyz&path=%2F%3Fed%3D2048#USCC
